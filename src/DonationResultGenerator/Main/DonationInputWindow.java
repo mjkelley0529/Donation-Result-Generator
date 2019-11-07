3f7804cf493bc64ab78d46f6f7d1a02b553291db
mkelley <xkelley0529@gmail.com>
2019-11-07 12:10:29 -0600

    5th Commit, implemented File Reader in place of hard coding.

--- src/DonationResultGenerator/Main/DonationInputWindow.java
+++ src/DonationResultGenerator/Main/DonationInputWindow.java
@@ -1,6 +1,6 @@
 package DonationResultGenerator.Main;
 
-import DonationResultGenerator.Resources.GlobalVariables;
+import DonationResultGenerator.Resources.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,7 +8,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Random;
+import java.util.Scanner;
 
 public class DonationInputWindow extends JFrame implements ActionListener, KeyListener {
     //Final Variables
@@ -89,49 +93,10 @@ public class DonationInputWindow extends JFrame implements ActionListener, KeyLi
         setVisible(true);
     }//DonationInputWindow
     //Game Logics
-    private ResultWindow runMinecraftLogic() {
+    private ResultWindow runMinecraftLogic() throws FileNotFoundException {
         //Variables
-        final String[][] OUTCOMMANDS = new String[3][10];
-        //Bad Commands
-        OUTCOMMANDS[0][0] = "/effect give @s slowness 20 1 true";
-        OUTCOMMANDS[0][1] = "/effect give @s blindness 20 1 true";
-        OUTCOMMANDS[0][2] = "/effect give @s poison 5 2 true";
-        OUTCOMMANDS[0][3] = "/summon zombie ~3 ~ ~3 {HandItems:[{Count:1,id:diamond_sword},{}]," +
-                "ArmorItems:[{},{},{Count:1,id:diamond_chestplate},{}],CustomName:\"\\\"Flying Zamboo\\\"\"," +
-                "HandDropChances:[0.1f,0.0f],ArmorDropChances:[0.0f,0.0f,0.1f,0.0f]}";
-        OUTCOMMANDS[0][4] = "/summon zombie ~-3 ~ ~-3 {HandItems:[{Count:1,id:diamond_sword},{}]," +
-                "ArmorItems:[{},{},{Count:1,id:diamond_chestplate},{}],CustomName:\"\\\"Flying Zamboo\\\"\"," +
-                "HandDropChances:[0.1f,0.0f],ArmorDropChances:[0.0f,0.0f,0.1f,0.0f]}";
-        OUTCOMMANDS[0][5] = "/summon creeper ~3 ~1 ~3 {powered:1,CustomName:\"\\\"DEAD\\\"\"}";
-        OUTCOMMANDS[0][6] = "/summon creeper ~-3 ~1 ~-3 {powered:1,CustomName:\"\\\"DEAD\\\"\"}";
-        OUTCOMMANDS[0][7] = "/summon creeper ~-3 ~1 ~-3 {Passengers:[{id:creeper,powered:1,ActiveEffects:[{Id:1,Amplifier:0,Duration:999999}]}]}";
-        OUTCOMMANDS[0][8] = "/summon creeper ~3 ~1 ~3 {Passengers:[{id:creeper,powered:1,ActiveEffects:[{Id:1,Amplifier:0,Duration:999999}]}]}";
-        OUTCOMMANDS[0][9] = "/kill @p";
-        //Good Commands
-        OUTCOMMANDS[1][0] = "/give @s coal_ore{display:{Name:\"\\\"Common Donator Ore\\\"\"," +
-                "Lore:[\"\\\"Just plain common ore. Should break.\\\"\"]}} 16";
-        OUTCOMMANDS[1][1] = "/give @s iron_ore{display:{Name:\"\\\"Uncommon Donator Ore\\\"\"," +
-                "Lore:[\"\\\"Better than common ore. But not by a lot.\\\"\"]}} 8";
-        OUTCOMMANDS[1][2] = "/give @s lapis_ore{display:{Name:\"\\\"Rare Donator Ore\\\"\"," +
-                "Lore:[\"\\\"Rare Donator Ore? You should feel special.\\\"\"]}} 4";
-        OUTCOMMANDS[1][3] = "/give @s redstone_ore{display:{Name:\"\\\"Rare Donator Ore\\\"\"," +
-                "Lore:[\"\\\"Rare Donator Ore? You should feel special.\\\"\"]}} 4";
-        OUTCOMMANDS[1][4] = "/give @s gold_ore{display:{Name:\"\\\"Rare Donator Ore\\\"\"," +
-                "Lore:[\"\\\"Rare Donator Ore? You should feel special.\\\"\"]}} 4";
-        OUTCOMMANDS[1][5] = "/give @s diamond_ore{display:{Name:\"\\\"Mythic Donator Ore\\\"\"," +
-                "Lore:[\"\\\"MYTHIC ORE!!!! ALL HAIL THE GOD OF RNG!!!!\\\"\"]}} 2";
-        OUTCOMMANDS[1][6] = "/give @s nether_quartz_ore{display:{Name:\"\\\"Mythic Donator Ore\\\"\"," +
-                "Lore:[\"\\\"MYTHIC ORE!!!! ALL HAIL THE GOD OF RNG!!!!\\\"\"]}} 2";
-        OUTCOMMANDS[1][7] = "/give @s diamond_sword{display:{Name:\"\\\"Sword of the Donator\\\"\"," +
-                "Lore:[\"\\\"A powerful weapon forged from the magic of RNG.\\\"\"]}," +
-                "Enchantments:[{id:sharpness,lvl:5},{id:looting,lvl:3}," +
-                "{id:sweeping,lvl:3},{id:unbreaking,lvl:3},{id:mending,lvl:1}]} 1";
-        OUTCOMMANDS[1][8] = "/give @s shield{display:{Name:\"\\\"Shield of the Donator\\\"\"," +
-                "Lore:[\"\\\"A powerful shield forged from the Magic of RNG.\\\"\"]}," +
-                "Enchantments:[{id:unbreaking,lvl:3},{id:mending,lvl:1}]," +
-                "BlockEntityTag:{Base:15,Patterns:[{Pattern:\"sku\",Color:10}]}} 1";
-        OUTCOMMANDS[1][9] = "/effect give @s health_boost 100000 1 true";
-        OUTCOMMANDS[2][0] = "/give @s diamond_block 64";
+        String[][] outCommands = readFile("CommandList.txt");
+        print(outCommands.length);
         double dA=1;
         try {
             dA=Double.parseDouble(input.getText());
@@ -141,61 +106,33 @@ public class DonationInputWindow extends JFrame implements ActionListener, KeyLi
         int[] out = {0, 0};
         Random random = new Random();
         //Logic
-        int i=1, randInt=0;
-        while(dA>=i) {
-            randInt=random.nextInt(i);
-            i++;
-        }
+        int randInt=random.nextInt((int)dA);
         if(randInt<=2) {
             out[0]=1;
         } else {
             out[0]=0;
         }
         randInt=random.nextInt(100)+1;
-        if(out[0]==0) {
-            if (randInt <= 40) {
-                out[1] = 0;
-            } else if(randInt>40&&randInt<=60) {
-                out[1]=1;
-            } else if(randInt>60&&randInt<=70) {
-                out[1]=2;
-            } else if(randInt>70&&randInt<=80) {
-                out[1]=3;
-            } else if(randInt>80&&randInt<=90) {
-                out[1]=4;
-            } else if(randInt>90&&randInt<=93) {
-                out[1]=5;
-            } else if(randInt>93&&randInt<=96) {
-                out[1]=6;
-            } else if(randInt==97) {
-                out[1]=7;
-            } else if(randInt==98) {
-                out[1]=8;
-            } else if(randInt>=99) {
-                out[1]=9;
-            }
-        } else {
-            if (randInt <= 40) {
-                out[1] = 0;
-            } else if(randInt<=60) {
-                out[1]=1;
-            } else if(randInt<=70) {
-                out[1]=2;
-            } else if(randInt<=80) {
-                out[1]=3;
-            } else if(randInt<=90) {
-                out[1]=4;
-            } else if(randInt<=93) {
-                out[1]=5;
-            } else if(randInt<=96) {
-                out[1]=6;
-            } else if(randInt==97) {
-                out[1]=7;
-            } else if(randInt==98) {
-                out[1]=8;
-            } else if(randInt>=99) {
-                out[1]=9;
-            }
+        if (randInt <= 40) {
+            out[1] = 0;
+        } else if(randInt<=60) {
+            out[1]=1;
+        } else if(randInt<=70) {
+            out[1]=2;
+        } else if(randInt<=80) {
+            out[1]=3;
+        } else if(randInt<=90) {
+            out[1]=4;
+        } else if(randInt<=93) {
+            out[1]=5;
+        } else if(randInt<=96) {
+            out[1]=6;
+        } else if(randInt==97) {
+            out[1]=7;
+        } else if(randInt==98) {
+            out[1]=8;
+        } else if(randInt>=99) {
+            out[1] = 9;
         }
         //Special Cases
         if(dA==69) {
@@ -203,18 +140,56 @@ public class DonationInputWindow extends JFrame implements ActionListener, KeyLi
             out[1]=0;
         }
         //ShowResults
-        return new ResultWindow("Execute this command:", OUTCOMMANDS[out[0]][out[1]], this, true);
+        return new ResultWindow("Execute this command:", outCommands[out[0]][out[1]], this, true);
     }//runMinecraftLogic
-    private ResultWindow runOverwatchLogic() {
+    private ResultWindow runOverwatchLogic() throws FileNotFoundException {
+        String[][] HeroList=readFile("HeroList.txt");
         return new ResultWindow(this);
     }//runOverwatchLogic
     private ResultWindow runKSPLogic() {
         return new ResultWindow(this);
     }//runKSPLogic
+    private String[][] readFile(String fileName) throws FileNotFoundException {
+        FileReader inFile=new FileReader(new File("src/DonationResultGenerator/Resources/"+fileName));
+        Scanner fileScan=new Scanner(inFile);
+        int dimX=fileScan.nextInt();
+        String outStrings[][]=new String [dimX][];
+        fileScan.nextLine();
+        for(int i=0;i<dimX;i++) {
+            String inString="";
+            int aIterations=0;
+            if(fileScan.hasNextLine()) {
+                inString=fileScan.nextLine();
+            }
+            if(inString.contains("<")&&inString.contains(">")) {
+                aIterations=Integer.parseInt(inString.substring(inString.indexOf('%')+1));
+                outStrings[i]=new String[aIterations];
+            }
+            for(int a=0;a<aIterations;a++) {
+                inString=fileScan.nextLine();
+                outStrings[i][a] = inString;
+            }
+        }
+        return outStrings;
+    }
     //Utility Methods
     private <T> void print(T printString) {
         System.out.println(printString);
     }//print
+    private <T> void print(T printString, boolean error) {
+        if(error) {
+            System.err.println(printString);
+        } else {
+            System.out.println(printString);
+        }
+    }//print
+    private void print(String[][] printStrings) {
+        for(int i=0;i<printStrings.length;i++) {
+            for(int a=0;a<printStrings[i].length;a++) {
+                print(printStrings[i][a]+" "+i+" "+a);
+            }
+        }
+    }
     public static String getJTitle() {
         return TITLE+" "+VERID;
     }//getTitle
@@ -223,23 +198,25 @@ public class DonationInputWindow extends JFrame implements ActionListener, KeyLi
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource() == b) {
             ResultWindow rw = null;
-            switch (gameString) {
-                case "Minecraft":
-                    rw = runMinecraftLogic();
-                    break;
-                case "Overwatch":
-                    rw = runOverwatchLogic();
-                    break;
-                case "Kerbal Space Program":
-                    rw = runKSPLogic();
-                    break;
-                default:
-                    break;
-            }
             try {
-                rw.reveal();
-            } catch (NullPointerException e) {
+                switch (gameString) {
+                    case "Minecraft":
+                        rw = runMinecraftLogic();
+                        break;
+                    case "Overwatch":
+                        rw = runOverwatchLogic();
+                        break;
+                    case "Kerbal Space Program":
+                        rw = runKSPLogic();
+                        break;
+                    default:
+                        break;
+                }
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
+            }
+            if(rw!=null) {
+                rw.reveal();
             }
             input.setText("");
         }
